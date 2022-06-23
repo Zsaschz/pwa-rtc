afa808f6839e5eb5ba3078e70842d58f074cd7f4
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-06-23 09:16:27 +0700

    feat: integrate liveness detection on server

--- socket-server/server.py
+++ socket-server/server.py
@@ -21,6 +21,25 @@ pcs = set()
 relay = MediaRelay()
 
 
+import cv2
+from tensorflow.keras.preprocessing.image import img_to_array
+import os
+import numpy as np
+from tensorflow.keras.models import model_from_json
+
+root_dir = os.getcwd()
+# Load Face Detection Model
+face_cascade = cv2.CascadeClassifier("models/haarcascade_frontalface_default.xml")
+# Load Anti-Spoofing Model graph
+json_file = open('antispoofing_models/antispoofing_model.json','r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+# load antispoofing model weights 
+model.load_weights('antispoofing_models/antispoofing_model.h5')
+print("Model loaded from disk")
+
+
 class VideoTransformTrack(MediaStreamTrack):
     """
     A video stream track that transforms frames from an another track.
@@ -35,60 +54,97 @@ class VideoTransformTrack(MediaStreamTrack):
 
     async def recv(self):
         frame = await self.track.recv()
-
-        if self.transform == "cartoon":
-            img = frame.to_ndarray(format="bgr24")
-
-            # prepare color
-            img_color = cv2.pyrDown(cv2.pyrDown(img))
-            for _ in range(6):
-                img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
-            img_color = cv2.pyrUp(cv2.pyrUp(img_color))
-
-            # prepare edges
-            img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            img_edges = cv2.adaptiveThreshold(
-                cv2.medianBlur(img_edges, 7),
-                255,
-                cv2.ADAPTIVE_THRESH_MEAN_C,
-                cv2.THRESH_BINARY,
-                9,
-                2,
-            )
-            img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-            # combine color and edges
-            img = cv2.bitwise_and(img_color, img_edges)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "edges":
-            # perform edge detection
-            img = frame.to_ndarray(format="bgr24")
-            img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
-        elif self.transform == "rotate":
-            # rotate image
-            img = frame.to_ndarray(format="bgr24")
-            rows, cols, _ = img.shape
-            M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
-            img = cv2.warpAffine(img, M, (cols, rows))
-
-            # rebuild a VideoFrame, preserving timing information
-            new_frame = VideoFrame.from_ndarray(img, format="bgr24")
-            new_frame.pts = frame.pts
-            new_frame.time_base = frame.time_base
-            return new_frame
+        if self.transform == "liveness-detection":
+            try:
+                img = frame.to_ndarray(format="bgr24")
+                # ret,frame = video.read()
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(gray,1.3,5)
+                for (x,y,w,h) in faces:  
+                    face = img[y-5:y+h+5,x-5:x+w+5]
+                    resized_face = cv2.resize(face,(160,160))
+                    resized_face = resized_face.astype("float") / 255.0
+                    # resized_face = img_to_array(resized_face)
+                    resized_face = np.expand_dims(resized_face, axis=0)
+                    # pass the face ROI through the trained liveness detector
+                    # model to determine if the face is "real" or "fake"
+                    preds = model.predict(resized_face)[0]
+                    print(preds)
+                    if preds> 0.5:
+                        label = 'spoof'
+                        cv2.putText(img, label, (x,y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+                        cv2.rectangle(img, (x, y), (x+w,y+h),
+                            (0, 0, 255), 2)
+                    else:
+                        label = 'real'
+                        cv2.putText(img, label, (x,y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0), 2)
+                        cv2.rectangle(img, (x, y), (x+w,y+h),
+                        (0, 255, 0), 2)
+                new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+                new_frame.pts = frame.pts
+                new_frame.time_base = frame.time_base
+                return new_frame
+            except Exception as e:
+                print(e)
+                return frame
         else:
             return frame
+
+        # if self.transform == "cartoon":
+        #     img = frame.to_ndarray(format="bgr24")
+
+        #     # prepare color
+        #     img_color = cv2.pyrDown(cv2.pyrDown(img))
+        #     for _ in range(6):
+        #         img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
+        #     img_color = cv2.pyrUp(cv2.pyrUp(img_color))
+
+        #     # prepare edges
+        #     img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        #     img_edges = cv2.adaptiveThreshold(
+        #         cv2.medianBlur(img_edges, 7),
+        #         255,
+        #         cv2.ADAPTIVE_THRESH_MEAN_C,
+        #         cv2.THRESH_BINARY,
+        #         9,
+        #         2,
+        #     )
+        #     img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
+
+        #     # combine color and edges
+        #     img = cv2.bitwise_and(img_color, img_edges)
+
+        #     # rebuild a VideoFrame, preserving timing information
+        #     new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+        #     new_frame.pts = frame.pts
+        #     new_frame.time_base = frame.time_base
+        #     return new_frame
+        # elif self.transform == "edges":
+        #     # perform edge detection
+        #     img = frame.to_ndarray(format="bgr24")
+        #     img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
+
+        #     # rebuild a VideoFrame, preserving timing information
+        #     new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+        #     new_frame.pts = frame.pts
+        #     new_frame.time_base = frame.time_base
+        #     return new_frame
+        # elif self.transform == "rotate":
+        #     # rotate image
+        #     img = frame.to_ndarray(format="bgr24")
+        #     rows, cols, _ = img.shape
+        #     M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
+        #     img = cv2.warpAffine(img, M, (cols, rows))
+
+        #     # rebuild a VideoFrame, preserving timing information
+        #     new_frame = VideoFrame.from_ndarray(img, format="bgr24")
+        #     new_frame.pts = frame.pts
+        #     new_frame.time_base = frame.time_base
+        #     return new_frame
+        # else:
+        #     return frame
 
 
 async def index(request):
@@ -115,7 +171,6 @@ async def offer(request):
     log_info("Created for %s", request.remote)
 
     # prepare local media
-    player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
     if args.record_to:
         recorder = MediaRecorder(args.record_to)
     else:
@@ -139,10 +194,7 @@ async def offer(request):
     def on_track(track):
         log_info("Track %s received", track.kind)
 
-        if track.kind == "audio":
-            pc.addTrack(player.audio)
-            recorder.addTrack(track)
-        elif track.kind == "video":
+        if track.kind == "video":
             pc.addTrack(
                 VideoTransformTrack(
                     relay.subscribe(track), transform=params["video_transform"]
