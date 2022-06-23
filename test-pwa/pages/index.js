504bf6fb66347716b2fb4a36711d0dbe9df88d4c
Muhammad Fathan Muthahhari <fathanmuthahhari@gmail.com>
2022-06-23 09:17:52 +0700

    refactor: remove unnecessary feature

--- test-pwa/pages/index.js
+++ test-pwa/pages/index.js
@@ -6,10 +6,6 @@ export default function Home() {
   // peer connection
   var pc = null;
 
-  // data channel
-  var dc = null,
-    dcInterval = null;
-
   function createPeerConnection() {
     var config = {
       sdpSemantics: "unified-plan",
@@ -53,17 +49,11 @@ export default function Home() {
         var offer = pc.localDescription;
         var codec;
 
-        codec = document.getElementById("audio-codec").value;
-        if (codec !== "default") {
-          offer.sdp = sdpFilterCodec("audio", codec, offer.sdp);
-        }
-
         codec = document.getElementById("video-codec").value;
         if (codec !== "default") {
           offer.sdp = sdpFilterCodec("video", codec, offer.sdp);
         }
 
-        document.getElementById("offer-sdp").textContent = offer.sdp;
         return fetch("http://localhost:8080/offer", {
           body: JSON.stringify({
             sdp: offer.sdp,
@@ -90,37 +80,10 @@ export default function Home() {
   function start() {
     pc = createPeerConnection();
 
-    var time_start = null;
-
-    function current_stamp() {
-      if (time_start === null) {
-        time_start = new Date().getTime();
-        return 0;
-      } else {
-        return new Date().getTime() - time_start;
-      }
-    }
-
-    if (document.getElementById("use-datachannel").checked) {
-      var parameters = JSON.parse(
-        document.getElementById("datachannel-parameters").value
-      );
-
-      dc = pc.createDataChannel("chat", parameters);
-      dc.onclose = function () {
-        clearInterval(dcInterval);
-      };
-      dc.onopen = function () {
-        dcInterval = setInterval(function () {
-          var message = "ping " + current_stamp();
-          dc.send(message);
-        }, 1000);
-      };
-    }
-
     var constraints = {
-      audio: document.getElementById("use-audio").checked,
+      audio: false,
       video: false,
+      frameRate: { max: 1 },
     };
 
     if (document.getElementById("use-video").checked) {
@@ -143,7 +106,13 @@ export default function Home() {
       navigator.mediaDevices.getUserMedia(constraints).then(
         function (stream) {
           stream.getTracks().forEach(function (track) {
-            pc.addTrack(track, stream);
+            let sender = pc.addTrack(track, stream);
+
+            // // get the current parameters first
+            // var params = sender.getParameters();
+
+            // params.encodings[0].maxBitrate = 6000;
+            // params.encodings[0].scaleResolutionDownBy = 2;
           });
           return negotiate();
         },
@@ -159,11 +128,6 @@ export default function Home() {
   }
 
   function stop() {
-    // close data channel
-    if (dc) {
-      dc.close();
-    }
-
     // close transceivers
     if (pc.getTransceivers) {
       pc.getTransceivers().forEach(function (transceiver) {
@@ -254,56 +218,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h2>Options</h2>
-        <div className="option">
-          <input id="use-datachannel" checked="checked" type="checkbox" />
-          <label htmlFor="use-datachannel">Use datachannel</label>
-          <select id="datachannel-parameters">
-            <option value='{"ordered": true}'>Ordered, reliable</option>
-            <option value='{"ordered": false, "maxRetransmits": 0}'>
-              Unordered, no retransmissions
-            </option>
-            <option value='{"ordered": false, "maxPacketLifetime": 500}'>
-              Unordered, 500ms lifetime
-            </option>
-          </select>
-        </div>
-        <div className="option">
-          <input id="use-audio" checked="checked" type="checkbox" />
-          <label htmlFor="use-audio">Use audio</label>
-          <select id="audio-codec">
-            <option value="default" selected>
-              Default codecs
-            </option>
-            <option value="opus/48000/2">Opus</option>
-            <option value="PCMU/8000">PCMU</option>
-            <option value="PCMA/8000">PCMA</option>
-          </select>
-        </div>
         <div className="option">
           <input id="use-video" type="checkbox" defaultChecked />
           <label htmlFor="use-video">Use video</label>
-          <select id="video-resolution">
-            <option value="" selected>
-              Default resolution
-            </option>
+          <select id="video-resolution" defaultValue={""}>
+            <option value="">Default resolution</option>
             <option value="320x240">320x240</option>
             <option value="640x480">640x480</option>
             <option value="960x540">960x540</option>
             <option value="1280x720">1280x720</option>
           </select>
-          <select id="video-transform">
-            <option value="none" selected>
-              No transform
-            </option>
-            <option value="edges">Edge detection</option>
-            <option value="cartoon">Cartoon effect</option>
-            <option value="rotate">Rotate</option>
+          <select id="video-transform" defaultValue={"none"}>
+            <option value="none">No transform</option>
+            <option value="liveness-detection">Liveness Detection</option>
           </select>
-          <select id="video-codec">
-            <option value="default" selected>
-              Default codecs
-            </option>
+          <select id="video-codec" defaultValue={"default"}>
+            <option value="default">Default codecs</option>
             <option value="VP8/90000">VP8</option>
             <option value="H264/90000">H264</option>
           </select>
